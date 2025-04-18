5f01256a58db88f061a90ca49af0b40b125ea65c
fayssaldd <fayssaldd982@gmail.com>
2025-04-18 17:30:25 +0100

    first commit for this project

---
+++ src/test/java/com/adapter/CalculatriceTest.java
@@ -0,0 +1,25 @@
+package com.adapter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatriceTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd(){
+        Assertions.assertEquals(3,calc.add(1,2));
+    }
+    @Test
+    void testSubtract(){
+        Assertions.assertEquals(4,calc.sub(9,5));
+    }
+    @Test
+    void testMultiply(){
+        Assertions.assertEquals(6, calc.mul(2,3));
+    }
+    @Test
+    void testDivide(){
+        Assertions.assertEquals(5, calc.div(10,2));
+    }
+}
